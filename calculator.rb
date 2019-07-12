d18197de0efd5747aa930a1765cc2209bea8e9dd
Jason Shockness <jshock12@gmail.com>
2019-07-12 03:55:25 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,9 @@
-first number = "Hello World!"
+# Add your variables here
+first_number = 12
+second_number = 10
+
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
+
